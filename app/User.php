ce0d9cfc8cf48d0fb09c4182376ca1c23cc3cacf
aziraismail <54127158+aziraismail@users.noreply.github.com>
2019-08-19 15:57:01 +0800

    Update User.php

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'emailAddress','userName','password','createdDate','updatedDate'
     ];
 
     /**
