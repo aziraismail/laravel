0b04e5fc33c86391051f17ee5ec56c618aaa0ac3
aziraismail <54127158+aziraismail@users.noreply.github.com>
2019-08-19 17:12:26 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,3 +11,39 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+
+class UserController extends Controller{
+  public function createUser(Request $request){
+ 
+      $user = User::create($request->all());
+ 
+      return response()->json($user);
+ 
+  }
+ 
+  public function updateUser(Request $request, $emailAdd){
+      $user  = User::find($emailAdd);
+      $user->username = $request->input('username');
+      $user->password = $request->input('password');
+      $user->createdDate = $request->input('createdDate');
+      $user->updatedDate = $request->input('updatedDate');
+      $user->save()
+ 
+      return response()->json($user);
+  }  
+  public function deleteUser($emailAdd){
+      $user  = User::find($emailAdd);
+      $user->delete();
+ 
+      return response()->json('Removed successfully.');
+  }
+  public function index(){
+ 
+      $users  = User::all();
+ 
+      return response()->json($users);
+ 
+  }
+}
+
+?>
